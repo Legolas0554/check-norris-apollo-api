3e63625702e148f2c1eca89da1c50624e7507dc6
GGebersohn <legolas0554@gmail.com>
2019-07-27 16:51:54 +0200

    update enviroments added endpoints

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -6,6 +6,10 @@ interface Environment {
     playground: boolean
   },
   port: number|string;
+  baseURL: string;
+  categoriesEndpoint: string;
+  randomEndpoint: string;
+  searchEndpoint: string;
 }
 
 export const environment: Environment = {
@@ -13,5 +17,9 @@ export const environment: Environment = {
     introspection: process.env.APOLLO_INTROSPECTION === 'true',
     playground: process.env.APOLLO_PLAYGROUND === 'true'
   },
-  port: process.env.PORT || defaultPort
+  port: process.env.PORT || defaultPort,
+  baseURL:'https://api.chucknorris.io/',
+  categoriesEndpoint:'/jokes/categories',
+  randomEndpoint: '/jokes/random',
+  searchEndpoint: '/jokes/search'
 };
