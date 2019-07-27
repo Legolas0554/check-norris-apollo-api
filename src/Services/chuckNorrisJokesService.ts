649835dd6036c6830deec73005969e29ab8b682f
GGebersohn <legolas0554@gmail.com>
2019-07-27 18:16:40 +0200

    added response model update service

---
+++ src/Services/chuckNorrisJokesService.ts
@@ -0,0 +1,37 @@
+import { JokesService } from "../interfaces/jokesService";
+import { RESTDataSource } from 'apollo-datasource-rest';
+import { environment } from "../enviroments/environment";
+import { ChuckNorrisJokeResponse} from "../models/response";
+
+
+
+export class ChuckNorrisJokesService extends RESTDataSource implements JokesService   {
+    
+    constructor() {
+        super();
+        this.baseURL = environment.baseURL;
+    }
+
+    async getCategories()   {
+        return this.get<string[]>(environment.categoriesEndpoint);
+    }  
+
+    async getRandomJoke() {
+        const result = await this.get<ChuckNorrisJokeResponse>(environment.randomEndpoint);
+
+        return result.value;
+    }
+
+    async getRandomJokeForCategory(category: string) {
+        const result = await this.get<ChuckNorrisJokeResponse>(environment.randomEndpoint, {category});
+
+        return result.value
+    }
+
+    async searchForJoke(search: string) {
+        const result = await this.get<ChuckNorrisJokeResponse>(environment.searchEndpoint, {search});
+
+        return result.value
+    }
+    
+}
--- src/interfaces/jokesService.ts
+++ src/interfaces/jokesService.ts
@@ -1,8 +1,12 @@
 import { RESTDataSource } from "apollo-datasource-rest";
 
 export interface Jokes extends RESTDataSource {
+    
     getCategories(): Promise<string[]>;
+
     getRandomJoke(): Promise<string>;
+
     getRandomJokeForCategory(category:string): Promise<string>;
+
     searchForJoke(search:string): Promise<string>;
 }
